bc704645a229311f2308a07b6ee5c40882ced6d1
Christian Svensson <blue@cmd.nu>
2013-08-20 15:44:02 +0200

    Initial support for OpenRISC
    This patch includes the OpenRISC glibc support for linux.
    
    YYYY-MM-DD  Stafford Horne  <shorne@gmail.com>
    	    Christian Svensson  <blue@cmd.nu>
    
    	* elf/elf.h: Add OpenRISC relocations.
    	* sysdeps/or1k/*: New files.
    	* sysdeps/unix/sysv/linux/or1k/*: New files.

---
+++ sysdeps/or1k/fpu/fesetenv.c
@@ -0,0 +1,37 @@
+/* Install given floating-point environment.
+   Copyright (C) 2020 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library.  If not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <fenv.h>
+#include <fpu_control.h>
+
+int
+__fesetenv (const fenv_t *envp)
+{
+  if (envp == FE_DFL_ENV)
+    _FPU_SETCW (_FPU_DEFAULT);
+  else if (envp == FE_NOMASK_ENV)
+    _FPU_SETCW (_FPU_IEEE);
+  else
+    _FPU_SETCW (*envp);
+
+  /* Success.  */
+  return 0;
+}
+libm_hidden_def (__fesetenv)
+weak_alias (__fesetenv, fesetenv)
+libm_hidden_weak (fesetenv)
