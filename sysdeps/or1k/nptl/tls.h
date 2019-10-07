4d05d82fd832e8e5389f6f41c463c4d773d2b80b
Stafford Horne <shorne@gmail.com>
2019-10-07 22:57:51 +0900

    or1l: fixup tls comments to be fsf style

--- sysdeps/or1k/nptl/tls.h
+++ sysdeps/or1k/nptl/tls.h
@@ -48,22 +48,22 @@ register tcbhead_t *__thread_self __asm__("r10");
 # include <sysdep.h>
 
 /* The TP points to the start of the TLS block.
- * As I understand it, this isn't strictly that "TP points to DTV" - it's
- * more where to place the TCB in the TLS block. This will place it in 
- * the beginning.
- *
- * Layout:
- *  ------------------------------------
- *  | PRE | TCB | TLS MEMORY ..        |
- *  ------------------------------------
- *              ^ r10 / TP
- *
- * PRE is the struct pthread described below
- * TCB is tcbhead_t
- * TLS memory is where the TLS program sections are loaded
- *
- * See _dl_allocate_tls_storage and __libc_setup_tls for more information.
- */
+   As I understand it, this isn't strictly that "TP points to DTV" - it's
+   more where to place the TCB in the TLS block. This will place it in 
+   the beginning.
+
+   Layout:
+    ------------------------------------
+    | PRE | TCB | TLS MEMORY ..        |
+   ------------------------------------
+                 ^ r10 / TP
+
+   PRE is the struct pthread described below
+   TCB is tcbhead_t
+   TLS memory is where the TLS program sections are loaded
+
+   See _dl_allocate_tls_storage and __libc_setup_tls for more information.  */
+
 # define TLS_DTV_AT_TP  1
 # define TLS_TCB_AT_TP  0
 
@@ -80,45 +80,45 @@ register tcbhead_t *__thread_self __asm__("r10");
 /* This is the size of the TCB.  */
 
 /* This is the size we need before TCB.
- * To support THREAD_GETMEM with friends we want to have a
- * struct pthread available.
- * Yank it in infront of everything, I'm sure nobody will mind.
- *
- * This memory is really allocated PRE the TLS block, so it's possible
- * to do ((char*)tlsblock) - TLS_PRE_TCB_SIZE to access it.
- * This is done for THREAD_SELF.  */
+   To support THREAD_GETMEM with friends we want to have a
+   struct pthread available.
+   Yank it in infront of everything, I'm sure nobody will mind.
+
+   This memory is really allocated PRE the TLS block, so it's possible
+   to do ((char*)tlsblock) - TLS_PRE_TCB_SIZE to access it.
+   This is done for THREAD_SELF.  */
+
 # define TLS_PRE_TCB_SIZE sizeof (struct pthread)
 
 
 /* Install the dtv pointer.
- * When called, dtvp is a pointer not the DTV per say (which should start
- * with the generation counter) but to the length of the DTV.
- * We can always index with -1, so we store dtvp[1]
- */
+   When called, dtvp is a pointer not the DTV per say (which should start
+   with the generation counter) but to the length of the DTV.
+   We can always index with -1, so we store dtvp[1].  */
+
 # define INSTALL_DTV(tcbp, dtvp) \
   (((tcbhead_t *) (tcbp))->dtv = (dtvp) + 1)
 
 /* Install new dtv for current thread
- * In a logicial world dtv here would also point to the length of the DTV.
- * However it does not, this time it points to the generation counter,
- * so just store it.
- *
- * Note: -1 is still valid and contains the length.  */
-# define INSTALL_NEW_DTV(dtv) \
-  (THREAD_DTV() = (dtv))
+   In a logicial world dtv here would also point to the length of the DTV.
+   However it does not, this time it points to the generation counter,
+   so just store it.
+
+   Note: -1 is still valid and contains the length.  */
+
+# define INSTALL_NEW_DTV(dtv) (THREAD_DTV() = (dtv))
 
 /* Return dtv of given thread descriptor.  */
-# define GET_DTV(tcbp) \
-  (((tcbhead_t *) (tcbp))->dtv)
+# define GET_DTV(tcbp) (((tcbhead_t *) (tcbp))->dtv)
 
 /* Code to initially initialize the thread pointer.
- *
- * Set TP to the address _after_ tcbhead_t. This will allow us
- * to change the size of tcbhead_t without having to re-link everything.
- *
- * This has to return NULL on success (or a string with the failure text).
- * It's hard to fail this, so return NULL always.
- */
+
+   Set TP to the address _after_ tcbhead_t. This will allow us
+   to change the size of tcbhead_t without having to re-link everything.
+
+   This has to return NULL on success (or a string with the failure text).
+   It's hard to fail this, so return NULL always.  */
+
 # define TLS_INIT_TP(tcbp) \
   ({__thread_self = ((tcbhead_t *)tcbp + 1); NULL;})
 
@@ -127,30 +127,32 @@ register tcbhead_t *__thread_self __asm__("r10");
   void *tp = ((char *) pd + TLS_PRE_TCB_SIZE + TLS_INIT_TCB_SIZE)
 
 /* Return the address of the dtv for the current thread.
- *
- * Dereference TP, offset to dtv - really straightforward.
- * Remember that we made TP point to after tcb, so we need to reverse that.
- */
+  
+   Dereference TP, offset to dtv - really straightforward.
+   Remember that we made TP point to after tcb, so we need to reverse that.  */
+
 #  define THREAD_DTV() \
   ((((tcbhead_t *)__thread_self)-1)->dtv)
 
 /* Return the thread descriptor for the current thread. 
- *
- * Return a pointer to the TLS_PRE area where we allocated space for
- * a struct pthread. Again, TP points to after tcbhead_t, compensate with
- * TLS_INIT_TCB_SIZE.
- *
- * I regard this is a seperate system from the "normal" TLS.
- */
+
+   Return a pointer to the TLS_PRE area where we allocated space for
+   a struct pthread. Again, TP points to after tcbhead_t, compensate with
+   TLS_INIT_TCB_SIZE.
+
+   I regard this is a seperate system from the "normal" TLS.  */
+
 # define THREAD_SELF \
   ((struct pthread *) ((char *) __thread_self - TLS_INIT_TCB_SIZE \
     - TLS_PRE_TCB_SIZE))
 
 /* Magic for libthread_db to know how to do THREAD_SELF.  */
+
 # define DB_THREAD_SELF \
   CONST_THREAD_AREA (32, sizeof (struct pthread))
 
 /* Access to data in the thread descriptor is easy.  */
+
 #define THREAD_GETMEM(descr, member) \
   descr->member
 #define THREAD_GETMEM_NC(descr, member, idx) \
@@ -161,6 +163,7 @@ register tcbhead_t *__thread_self __asm__("r10");
   descr->member[idx] = (value)
 
 /* Get and set the global scope generation counter in struct pthread.  */
+
 #define THREAD_GSCOPE_IN_TCB      1
 #define THREAD_GSCOPE_FLAG_UNUSED 0
 #define THREAD_GSCOPE_FLAG_USED   1
