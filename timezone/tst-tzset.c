2f3907d26483b7060155e9ccaf397f8f593480c1
Stafford Horne <shorne@gmail.com>
2021-09-20 06:03:07 +0900

    tst-tzset: output reason when creating 4GiB file fails
    Currently, if the temporary file creation fails the create_tz_file
    function returns NULL.  The NULL pointer is then passed to setenv which
    causes a SIGSEGV.  Rather than failing with a SIGSEGV print a warning
    and exit.

--- timezone/tst-tzset.c
+++ timezone/tst-tzset.c
@@ -103,6 +103,13 @@ static void
 test_tz_file (off64_t size)
 {
   char *path = create_tz_file (size);
+  if (path == NULL)
+   {
+     printf ("creating timezone file of size: %lld MiB failed.\n",
+	     size / (1024 * 1024));
+     exit (1);
+   }
+
   if (setenv ("TZ", path, 1) < 0)
     {
       printf ("setenv failed: %m\n");
