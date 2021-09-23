82c273b3c9a4c4d9005a2a6cf450f79a85801cc0
Stafford Horne <shorne@gmail.com>
2021-09-24 06:21:41 +0900

    ldso: Fixup comments and ifdef file identifier

--- sysdeps/or1k/ldsodefs.h
+++ sysdeps/or1k/ldsodefs.h
@@ -1,5 +1,5 @@
 /* Run-time dynamic linker data structures for loaded ELF shared objects.
-   Copyright (C) 2020 Free Software Foundation, Inc.
+   Copyright (C) 2021 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -16,8 +16,8 @@
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
 
-#ifndef	_SH_LDSODEFS_H
-#define	_SH_LDSODEFS_H	1
+#ifndef	_OR1K_LDSODEFS_H
+#define	_OR1K_LDSODEFS_H	1
 
 #include <elf.h>
 
