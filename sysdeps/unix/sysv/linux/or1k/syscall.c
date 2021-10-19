aff976b6975c386e74554a991bbc4fcc30e4eb74
Stafford Horne <shorne@gmail.com>
2021-10-20 06:51:52 +0900

    or1k: Linux Syscall Interface

---
+++ sysdeps/unix/sysv/linux/or1k/syscall.c
@@ -0,0 +1,45 @@
+/* System call interface.  OpenRISC version.
+   Copyright (C) 2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library.  If not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <sysdep.h>
+#include <stdarg.h>
+
+long int
+syscall (long int syscall_number, ...)
+{
+  unsigned long int arg1, arg2, arg3, arg4, arg5, arg6;
+  va_list arg;
+  long int ret;
+
+  va_start (arg, syscall_number);
+  arg1 = va_arg (arg, unsigned long int);
+  arg2 = va_arg (arg, unsigned long int);
+  arg3 = va_arg (arg, unsigned long int);
+  arg4 = va_arg (arg, unsigned long int);
+  arg5 = va_arg (arg, unsigned long int);
+  arg6 = va_arg (arg, unsigned long int);
+  va_end (arg);
+
+  ret = INTERNAL_SYSCALL_NCS (syscall_number, 6, arg1, arg2, arg3, arg4,
+			      arg5, arg6);
+
+  if (INTERNAL_SYSCALL_ERROR_P (ret))
+    return __syscall_error (ret);
+
+  return ret;
+}
