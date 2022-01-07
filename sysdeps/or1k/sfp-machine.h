b48beb9f1920cf19d9e1d3794405cca2ecd9a526
Stafford Horne <shorne@gmail.com>
2022-01-07 23:58:47 +0900

    or1k: Add hard float support
    This adds hardware floating support to OpenRISC.  There are 2 main parts
    to this patch.
    
     - Implement fenv functions to update the FPCSR flags.
     - Update machine context functions to store and restore the FPCSR
       state.
    
    Note on adding __fpcsr to mcontext_t.  This is an ABI change, but also
    an ABI fix.  The linux kernel has always defined padding in mcontext_t
    that was was missing from the glibc ABI.  This unused space is has now
    been re-used for storing the FPCSR.  This patch brings OpenRISC glibc in
    line with the Linux kernel and other libc implementation (musl).
    
    I have tested this patch both with hard-float and soft-float builds and
    the test results are as follows:
    
    Hard Float
    
        # failures
        FAIL: elf/tst-sprof-basic      (Not checked, see in other ports too)
        FAIL: gmon/tst-gmon-pie        (GCC PIE bug)
        FAIL: gmon/tst-gmon-pie-gprof  (GCC PIE bug)
        FAIL: nptl/tst-cond24          (timeouts)
        FAIL: nptl/tst-mutex10         (timeouts)
    
        # summary
    	  5 FAIL
           4279 PASS
    	 83 UNSUPPORTED
    	 16 XFAIL
    	  2 XPASS
    
        # versions
        Toolchain: or1k-smhfpu-linux-gnu
        Compiler:  gcc version 14.0.1 20240324 (experimental) [master r14-9649-gbb04a11418f] (GCC)
        Binutils:  GNU assembler version 2.42.0 (or1k-smhfpu-linux-gnu) using BFD version (GNU Binutils) 2.42.0.20240324
        Linux:     Linux buildroot 6.8.0-rc5-00003-g9158c6253560-dirty #78 SMP Fri Mar 22 16:56:54 GMT 2024 openrisc GNU/Linux
    
    Soft Float
    
        # failures
        FAIL: elf/tst-sprof-basic
        FAIL: gmon/tst-gmon-pie
        FAIL: gmon/tst-gmon-pie-gprof
        FAIL: iconvdata/iconv-test
        FAIL: nptl/tst-mutex10
    
        # summary
    	  5 FAIL
           4281 PASS
    	 81 UNSUPPORTED
    	 16 XFAIL
    	  2 XPASS
    
        # versions
        Toolchain: or1k-smh-linux-gnu
        Compiler:  gcc version 14.0.1 20240324 (experimental) [master r14-9649-gbb04a11418f] (GCC)
        Binutils:  GNU assembler version 2.42.0 (or1k-smh-linux-gnu) using BFD version (GNU Binutils) 2.42.0.20240324
        Linux:     Linux buildroot 6.8.0-rc5-00003-g9158c6253560-dirty #78 SMP Fri Mar 22 16:56:54 GMT 2024 openrisc GNU/Linux

--- sysdeps/or1k/sfp-machine.h
+++ sysdeps/or1k/sfp-machine.h
@@ -36,7 +36,6 @@
 #define _FP_MUL_MEAT_DW_Q(R,X,Y)				\
   _FP_MUL_MEAT_DW_4_wide(_FP_WFRACBITS_Q,R,X,Y,umul_ppmm)
 
-
 #define _FP_DIV_MEAT_S(R,X,Y)	_FP_DIV_MEAT_1_loop(S,R,X,Y)
 #define _FP_DIV_MEAT_D(R,X,Y)	_FP_DIV_MEAT_2_udiv(D,R,X,Y)
 #define _FP_DIV_MEAT_Q(R,X,Y)	_FP_DIV_MEAT_4_udiv(Q,R,X,Y)
@@ -89,5 +88,22 @@
   unsigned int _fpcsr __attribute__ ((unused)) = FP_RND_NEAREST
 
 #define FP_ROUNDMODE (_fpcsr & FP_RND_MASK)
+
+#ifdef __or1k_hard_float__
+#define FP_INIT_ROUNDMODE					\
+do {								\
+  __asm__ volatile ("l.mfspr %0,r0,20" : "=r" (_fpcsr));	\
+} while (0)
+
+#define FP_HANDLE_EXCEPTIONS					\
+do {								\
+  if (__builtin_expect (_fex, 0))				\
+    {								\
+      _fpcsr &= ~FP_EX_ALL;					\
+      _fpcsr |= _fex;						\
+      __asm__ volatile ("l.mtspr r0,%0,20" : : "r" (_fpcsr));	\
+    }								\
+} while (0)
+#endif /* __or1k_hard_float__ */
 
 #define _FP_TININESS_AFTER_ROUNDING 0
